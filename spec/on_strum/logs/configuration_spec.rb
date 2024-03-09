963e2671d02fc621e2eb8a1a18885b7fe48d031d
Vladislav Trotsenko <admin@bestweb.com.ua>
2024-03-09 19:17:29 +0100

    Feature/Ability to configure builtin attribute key names
    * Added ability to configure builtin attribute key names
    * Added OnStrum::Logs::Configuration#log_attributes_order, tests
    * Updated OnStrum::Logs::Configuration, tests
    * Updated OnStrum::Logs::Logger::Default, tests
    * Updated OnStrum::Logs::Formatter::Base, tests
    * Updated OnStrum::Logs::Formatter::Json, tests
    * Updated OnStrum::Logs::RspecHelper::Configuration, tests
    * Updated OnStrum::Logs::RspecHelper::ContextGenerator, tests

--- spec/on_strum/logs/configuration_spec.rb
+++ spec/on_strum/logs/configuration_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe OnStrum::Logs::Configuration do
   describe 'defined constants' do
     it { expect(described_class).to be_const_defined(:INCOMPLETE_CONFIG) }
     it { expect(described_class).to be_const_defined(:SETTERS) }
+    it { expect(described_class).to be_const_defined(:BUILTIN_FIELDS_DEFAULT_NAMES) }
   end
 
   describe '.new' do
@@ -11,6 +12,12 @@ RSpec.describe OnStrum::Logs::Configuration do
     let(:detailed_formatter) { true }
     let(:service_name) { random_service_name }
     let(:service_version) { random_semver }
+    let(:field_name_level) { random_field_name }
+    let(:field_name_time) { random_field_name }
+    let(:field_name_message) { random_field_name }
+    let(:field_name_context) { random_field_name }
+    let(:field_name_exception_message) { random_field_name }
+    let(:field_name_exception_stack_trace) { random_field_name }
 
     context 'when valid configuration' do
       subject(:configuration) do
@@ -18,7 +25,13 @@ RSpec.describe OnStrum::Logs::Configuration do
           custom_formatter: custom_formatter,
           detailed_formatter: detailed_formatter,
           service_name: service_name,
-          service_version: service_version
+          service_version: service_version,
+          field_name_level: field_name_level,
+          field_name_time: field_name_time,
+          field_name_message: field_name_message,
+          field_name_context: field_name_context,
+          field_name_exception_message: field_name_exception_message,
+          field_name_exception_stack_trace: field_name_exception_stack_trace
         )
       end
 
@@ -27,6 +40,12 @@ RSpec.describe OnStrum::Logs::Configuration do
         expect(configuration.detailed_formatter).to eq(detailed_formatter)
         expect(configuration.service_name).to eq(service_name)
         expect(configuration.service_version).to eq(service_version)
+        expect(configuration.field_name_level).to eq(field_name_level)
+        expect(configuration.field_name_time).to eq(field_name_time)
+        expect(configuration.field_name_message).to eq(field_name_message)
+        expect(configuration.field_name_context).to eq(field_name_context)
+        expect(configuration.field_name_exception_message).to eq(field_name_exception_message)
+        expect(configuration.field_name_exception_stack_trace).to eq(field_name_exception_stack_trace)
         expect(configuration).to be_complete
       end
     end
@@ -76,6 +95,22 @@ RSpec.describe OnStrum::Logs::Configuration do
 
         include_examples 'raies argument error'
       end
+
+      OnStrum::Logs::Configuration::SETTERS[3..-1].each do |field_name_setter|
+        context "when argument #{field_name_setter}= invalid" do
+          subject(:configuration) do
+            create_configuration(
+              service_name: random_service_name,
+              service_version: random_semver,
+              field_name_setter => invalid_argument
+            )
+          end
+
+          let(:expected_error_message) { "#{invalid_argument} is not a valid #{field_name_setter}=" }
+
+          include_examples 'raies argument error'
+        end
+      end
     end
 
     context 'when configuration without block' do
@@ -85,6 +120,12 @@ RSpec.describe OnStrum::Logs::Configuration do
         expect(configuration.service_name).to be_nil
         expect(configuration.service_version).to be_nil
         expect(configuration.custom_formatter).to be_nil
+        expect(configuration.field_name_level).to eq(:level)
+        expect(configuration.field_name_time).to eq(:time)
+        expect(configuration.field_name_message).to eq(:message)
+        expect(configuration.field_name_context).to eq(:context)
+        expect(configuration.field_name_exception_message).to eq(:message)
+        expect(configuration.field_name_exception_stack_trace).to eq(:stack_trace)
         expect(configuration.detailed_formatter).to be_nil
         expect(configuration).not_to be_complete
       end
@@ -154,6 +195,32 @@ RSpec.describe OnStrum::Logs::Configuration do
       let(:expected_formatter) { use_formatter(:custom) }
 
       include_examples 'returns target formatter'
+    end
+  end
+
+  describe '#log_attributes_order' do
+    subject(:log_attributes_order) { configuration_instance.log_attributes_order }
+
+    let(:configuration_instance) do
+      create_configuration(
+        field_name_level: random_field_name,
+        field_name_time: random_field_name,
+        field_name_message: random_field_name,
+        field_name_context: random_field_name
+      )
+    end
+
+    it 'returns log attributes order' do
+      expect(log_attributes_order).to eq(
+        [
+          configuration_instance.field_name_level,
+          configuration_instance.field_name_time,
+          configuration_instance.field_name_message,
+          configuration_instance.field_name_context,
+          :service_name,
+          :service_version
+        ]
+      )
     end
   end
 end
--- spec/on_strum/logs/formatter/json_spec.rb
+++ spec/on_strum/logs/formatter/json_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe OnStrum::Logs::Formatter::Json do
   describe '.call' do
     subject(:formatter) { described_class.call(**log_data) }
 
+    let(:field_name_level) { :a }
+    let(:field_name_time) { :b }
+    let(:field_name_message) { :c }
+    let(:field_name_context) { :d }
     let(:level) { random_log_level }
     let(:time) { ::Time.new }
     let(:time_formatted) { time.strftime(described_class::DATETIME_FORMAT) }
@@ -15,27 +19,36 @@ RSpec.describe OnStrum::Logs::Formatter::Json do
     let(:service_version) { random_semver }
     let(:log_data) do
       [
-        [:level, level],
+        [field_name_level, level],
         [:service_name, service_name],
         [:service_version, service_version],
-        [:time, time],
-        [:message, message],
-        [:context, context]
+        [field_name_time, time],
+        [field_name_message, message],
+        [field_name_context, context]
       ].shuffle.to_h
+    end
+
+    before do
+      init_configuration(
+        field_name_level: field_name_level,
+        field_name_time: field_name_time,
+        field_name_message: field_name_message,
+        field_name_context: field_name_context
+      )
     end
 
     it 'renders arranged log data as json' do
       expect(described_class)
         .to receive(:arrange_attrs)
-        .with(log_data.merge(time: time_formatted))
+        .with(log_data.merge(field_name_time => time_formatted))
         .and_call_original
       expect(formatter).to eq(
         string_with_new_line(
           {
-            level: level,
-            time: time_formatted,
-            message: message,
-            context: context,
+            field_name_level => level,
+            field_name_time => time_formatted,
+            field_name_message => message,
+            field_name_context => context,
             service_name: service_name,
             service_version: service_version
           }.to_json
